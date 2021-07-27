5d7589ba2bf6fdc44f44cc03c0d19012034169ae
CHABAT ESTELLE <chabat.estelle@gmail.com>
2021-07-27 13:24:15 +0200

    Adding getpicture into project routes

--- src/api/projects/routes.js
+++ src/api/projects/routes.js
@@ -5,9 +5,11 @@ const router = express.Router();
 const getAll = require("./controllers/getAll");
 const getOne = require("./controllers/getOne");
 const getStacks = require("./controllers/getStacks");
+const getPictures = require("./controllers/getPictures");
 
 router.get("/", getAll);
 router.get("/:id", getOne);
 router.get("/:id/stacks", getStacks);
+router.get("/:id/pictures", getPictures);
 
 module.exports = router;
